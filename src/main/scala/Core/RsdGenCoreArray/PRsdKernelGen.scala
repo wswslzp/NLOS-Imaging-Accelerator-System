7ca212e3099b195cfd61fd3a58d3d5de3d835948
Zhengpeng Liao <wswslzp@outlook.com>
2020-11-21 10:47:26 +0800

    update daily

--- src/main/scala/Core/RsdGenCoreArray/PRsdKernelGen.scala
+++ src/main/scala/Core/RsdGenCoreArray/PRsdKernelGen.scala
@@ -37,10 +37,16 @@ case class PRsdKernelGen(cfg: RsdKernelConfig) extends Component {
   val delta_rsd_kernel_val_r = RegNext(delta_rsd_kernel_val)
   delta_rsd_kernel_val_r.foreach(_.init(HC(0, 0, io.rsd_prev.head.config)))
 
-  val rsd_prev_r = RegNextWhen(
-    next = io.rsd_prev,
-    cond = RegNext(io.impulse_valid, False)
-  )
+//  val rsd_prev_r = RegNextWhen(
+//    next = io.rsd_prev,
+//    cond = RegNext(io.impulse_valid, False)
+//  )
+  val rsd_prev_r = Reg(cloneOf(io.rsd_prev))
+  when(RegNext(io.impulse_valid, False)){
+    rsd_prev_r := io.rsd_prev
+  }otherwise {
+    rsd_prev_r.foreach(_ := HC(0, 0, io.rsd_prev.head.config))
+  }
   rsd_prev_r.foreach(_.init(HC(0, 0, io.rsd_prev.head.config)))
   for(idx <- io.rsd_next.indices){
     io.rsd_next(idx) := rsd_prev_r(idx) + delta_rsd_kernel_val_r(idx)
--- src/test/scala/RsdKenelGenTest/RsdGenCoreArrayMain.scala
+++ src/test/scala/RsdKenelGenTest/RsdGenCoreArrayMain.scala
@@ -283,7 +283,7 @@ object RsdGenCoreArrayMain extends App{
   write_image(uout_abs_max_flip, "tb/RsdGenCoreArray/nlos_hard_out.jpg")
 
   if(withWave){
-    Process("vcd2fsdb tb/RsdGenCoreArray/RsdGenCoreArray_tb.vcd -o tb/RsdGenCoreArray/RsdGenCoreArray_tb.fsdb").!
+    Process("vcd2fsdb tb/RsdGenCoreArray/RsdGenCoreArray_tb.vcd -o tb/RsdGenCoreArray/RsdGenCoreArray_tb.fsdb").!<
     Process("verdi -ssf tb/RsdGenCoreArray/RsdGenCoreArray_tb.fsdb").!!
   }
 
