d90feaa1a5d88c2ca0a0ba6cd3d1d3ce6b629d7a
Zhengpeng Liao <wswslzp@outlook.com>
2020-11-23 19:47:44 +0800

    update daily

--- src/test/scala/RsdKenelGenTest/RsdGenCoreArrayMain.scala
+++ src/test/scala/RsdKenelGenTest/RsdGenCoreArrayMain.scala
@@ -7,9 +7,9 @@ import Core.RsdGenCoreArray._
 import Config.RsdKernelConfig._
 
 object RsdGenCoreArrayMain extends App {
-  new java.io.File("rtl/RsdGenCoreArray").mkdirs()
+  new java.io.File("../rtl/RsdGenCoreArray").mkdirs()
   SpinalConfig(
-    targetDirectory = "rtl/RsdGenCoreArray",
+    targetDirectory = "../rtl/RsdGenCoreArray",
     oneFilePerComponent = true
   ).generateVerilog(
     RsdGenCoreArray(rsd_cfg, init_addr = 0)
