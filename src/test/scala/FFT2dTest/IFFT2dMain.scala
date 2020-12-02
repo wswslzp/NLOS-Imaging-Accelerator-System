fcced9f6806d0f0ddc9bc3129617220e1c9263c4
Zhengpeng Liao <wswslzp@outlook.com>
2020-12-02 10:08:08 +0800

    update daily

--- src/test/scala/FFT2dTest/IFFT2dMain.scala
+++ src/test/scala/FFT2dTest/IFFT2dMain.scala
@@ -10,8 +10,10 @@ object IFFT2dMain extends App{
     point = 128,
     row = 128
   )
+  new java.io.File("../rtl/IFFT2d").mkdir()
   val config = SpinalConfig(
-    targetDirectory = "../rtl/IFFT2d"
+    targetDirectory = "../rtl/IFFT2d",
+    oneFilePerComponent = true
   )
   SpinalVerilog(config)(
     IFFT2d(fft_config)
