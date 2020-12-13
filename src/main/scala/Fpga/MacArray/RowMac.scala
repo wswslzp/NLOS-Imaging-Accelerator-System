ed1e4cde41c75d41bfb3bebf5034d79c06f60f74
Zhengpeng Liao <wswslzp@outlook.com>
2020-12-13 19:37:06 +0800

    update daily

--- src/main/scala/Fpga/MacArray/RowMac.scala
+++ src/main/scala/Fpga/MacArray/RowMac.scala
@@ -11,18 +11,19 @@ case class RowMac(cfg: RsdKernelConfig) extends Component {
     val data_in = slave(Flow(HComplex(complex_cfg)))
     val data_out = out(HComplex(complex_cfg))
     val clear = in Bool()
-    val col_addr = in UInt(log2Up(cfg.cols) bit)
+    val acc_in_addr = in UInt(log2Up(cfg.cols) bit)
+    val pipe_out_addr = in UInt(log2Up(cfg.cols) bit)
   }
 
-  // TODO: Memory has problem of decoding HComplex?
+  // Note: Dual port SRAM/BRAM.
   val row_mem = Mem(Bits(complex_cfg.getComplexWidth bit), BigInt(cfg.cols)).init(Array.fill(cfg.cols)(B(0)))
   val prev_data = HComplex(complex_cfg)
-  prev_data := row_mem(io.col_addr)
+  prev_data := row_mem(io.acc_in_addr)
   when(io.data_in.valid) {
-    row_mem(io.col_addr) := (prev_data + io.data_in.payload).fixTo(complex_cfg).asBits
+    row_mem(io.acc_in_addr) := (prev_data + io.data_in.payload).fixTo(complex_cfg).asBits
   } elsewhen(io.clear){
-    row_mem(io.col_addr) := B(0, complex_cfg.getComplexWidth bit)
+    row_mem(io.pipe_out_addr) := 0
   }
-  io.data_out := row_mem(io.col_addr)
+  io.data_out := row_mem(io.pipe_out_addr)
 
 }
--- src/main/scala/Fpga/MacArray/RowMacArray.scala
+++ src/main/scala/Fpga/MacArray/RowMacArray.scala
@@ -17,21 +17,31 @@ case class RowMacArray(cfg: RsdKernelConfig) extends Component {
   // Pipeline the complex multiplication for good timing. Trade off between area and timing.
   val rsd_fft_prod = Vec.tabulate(cfg.rows){idx=>
     val tmp = io.rsd_kernel.payload(idx) * io.fft_out.payload(idx)
-    //    RegNext(tmp.fixTo(cfg.getMACDatConfig))
     RegNext(tmp)
   }
   val rsd_fft_prod_valid = RegNext(valid) init False
 
   // Count up for the current column address
-  val col_addr_area = countUpInside(rsd_fft_prod_valid, cfg.rows)
+  val col_addr_area = countUpInside(rsd_fft_prod_valid, cfg.cols)
   val col_addr = col_addr_area.cnt
   col_addr.setName("col_addr")
 
   // *************** MAC array ***********************
+  // Different from register array. When rsd fft prod valid is asserted,
+  //  mac array is also pipe out old data. So it needs two read out port and two write port.
+  //  One pair of WR port, is used to read previous data and write the accumulate data into
+  //  the same place. Another pair of WR port, is used to read out the data and clear the data
+  //  at the same time/place. The two pair of transaction happens in different phase in most of time
+  //  but at f0, they overlaps.
   val mac_array = Array.fill(cfg.rows)(RowMac(cfg))
   for(r <- cfg.rowRange){
     mac_array(r).io.data_in.valid := rsd_fft_prod_valid
     mac_array(r).io.data_in.payload := rsd_fft_prod(r)
+    when(rsd_fft_prod_valid){
+      mac_array(r).io.acc_in_addr := col_addr.value
+    } otherwise {
+      mac_array(r).io.acc_in_addr := col_addr.value
+    }
   }
 
   // **************** pipe out logic ****************
@@ -43,12 +53,10 @@ case class RowMacArray(cfg: RsdKernelConfig) extends Component {
   io.mac_result.valid := pipe_out_en
 
   for(r <- cfg.rowRange){
-    when(rsd_fft_prod_valid){
-      mac_array(r).io.col_addr := col_addr.value
-    } elsewhen(pipe_out_en) {
-      mac_array(r).io.col_addr := RegNext(pipe_out_col_addr)
+    when(pipe_out_en){
+      mac_array(r).io.pipe_out_addr := pipe_out_col_addr
     } otherwise {
-      mac_array(r).io.col_addr := 0
+      mac_array(r).io.pipe_out_addr := 0
     }
 
     mac_array(r).io.clear := pipe_out_en
