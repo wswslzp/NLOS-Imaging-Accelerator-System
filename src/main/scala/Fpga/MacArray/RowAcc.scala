2648eba2f636ca4f0656918376eecd3af9eac161
Zhengpeng Liao <wswslzp@outlook.com>
2020-12-14 17:13:51 +0800

    update daily

--- src/main/scala/Fpga/MacArray/RowAcc.scala
+++ src/main/scala/Fpga/MacArray/RowAcc.scala
@@ -13,8 +13,7 @@ case class RowAcc(cfg: HComplexConfig, cols: Int) extends Component {
     val acc_in_addr = in UInt(log2Up(cols) bit)
     val pipe_out_addr = in UInt(log2Up(cols) bit)
   }
-  val acc_in_addr_1 = RegNext(io.acc_in_addr) init 0
-  val pipe_out_addr_1 = RegNext(io.pipe_out_addr) init 0
+  val acc_in_addr_1 = RegNext(io.acc_in_addr) init U(0)
 
   // Note: Dual port SRAM/BRAM.
   val row_mem = Mem(Bits(cfg.getComplexWidth bit), BigInt(cols)).init(Array.fill(cols)(B(0)))
@@ -22,16 +21,16 @@ case class RowAcc(cfg: HComplexConfig, cols: Int) extends Component {
 
   // Get the previous data
   val prev_data = HComplex(cfg)
-//  prev_data := row_mem(io.acc_in_addr)
   prev_data := row_mem.readSync(io.acc_in_addr)
 
   val data_in = io.data_in.toReg(HC(0, 0, cfg))
   val sum = ( prev_data + data_in ).fixTo(cfg).asBits
-  val sum_1 = RegNext(sum) init B(0, cfg.getComplexWidth bit)
-  val data_in_valid_1 = RegNext(io.data_in.valid) init False
+//  val sum_1 = RegNext(sum) init B(0, cfg.getComplexWidth bit)
+  val data_in_valid_1 = Delay(io.data_in.valid, 1, init = False)
 
   when(data_in_valid_1) {
-    row_mem(acc_in_addr_1) := sum_1
+//    row_mem(acc_in_addr_1) := sum_1
+    row_mem(acc_in_addr_1) := sum
   }
 
   // When clear assert, all the pixel in row clears
