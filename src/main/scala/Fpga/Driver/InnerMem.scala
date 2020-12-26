fe65579de5e9f449447d1dfc553d55e3a4e1f2f6
Zhengpeng Liao <wswslzp@outlook.com>
2020-12-26 21:56:40 +0800

    update daily

--- src/main/scala/Fpga/Driver/InnerMem.scala
+++ src/main/scala/Fpga/Driver/InnerMem.scala
@@ -12,7 +12,8 @@ class InnerMem(memDepth: Int, width: Int) extends Area{ innerMem=>
 
   def addrIncr(): Unit = romAddr.increment()
 
-  def oneShotDriverFSM(addr: UInt, bus: Axi4WriteOnly) = new StateMachine {
+  def oneShotDriverFSM(addr: UInt, bus: Axi4WriteOnly, fsm_name: String) = new StateMachine {
+    setCompositeName(innerMem, s"${fsm_name}_fsm")
     val one_shot = new State
     val done_addr_shot = new State
     val done_data_shot = new State
@@ -77,97 +78,14 @@ class InnerMem(memDepth: Int, width: Int) extends Area{ innerMem=>
 
   /**
    * function of FSM to drive data into AXI4 bus in burst mode
-   * @param addrs Burst transaction initial addresses, which are the addresses of the slave memory space
-   * @param bus The master bus
-   * @param shot_len The length of burst transaction
-   * @return return the corresponding FSM
-   */
-  @deprecated
-  def burstDriverFSM(addrs: Vec[UInt], bus: Axi4WriteOnly, shot_len: Int) = new StateMachine {
-    //todo: Do burst transaction driver fsm
-    val burst_cnt = Counter(0 until addrs.length).setCompositeName(innerMem, "burst_cnt") // counter for the index burst shot
-    val shot_cnt = Counter(0 until shot_len).setCompositeName(innerMem, "shot_cnt") // counter for current index inside a burst transaction
-
-    val one_burst_shot = new State
-    val done_addr_shot = new State
-    val done_data_shot = new State
-    val addr_shot = new State with EntryPoint {
-      whenIsActive {
-        // memory part
-        en.set()
-        addrIncr()
-
-        // address channel
-        bus.aw.valid.set()
-        bus.aw.addr := addrs(burst_cnt.value.resized).resized
-        bus.aw.len := U(shot_len-1).resized
-        when(bus.aw.fire){goto(one_burst_shot)}
-      }
-    }
-
-    one_burst_shot
-      .whenIsActive {
-        en.set()
-
-        when(shot_cnt.willOverflow) {
-          burst_cnt.increment()
-          when(burst_cnt.willOverflowIfInc){
-            goto(done_addr_shot)
-          } otherwise{
-            goto(addr_shot)
-          }
-        } otherwise {
-          when(bus.w.fire){
-            addrIncr()
-          }
-        }
-
-        when(bus.w.fire){
-          shot_cnt.increment()
-        }
-
-        // address channel
-        bus.aw.valid.clear()
-        // data channel
-        bus.w.valid.set()
-        bus.w.data := data.resized
-        bus.w.last := shot_cnt.willOverflow
-      }
-
-    done_addr_shot
-      .whenIsActive {
-        en.clear()
-        // address channel
-        bus.aw.valid.set()
-        bus.aw.addr := ( addrs.last + 1 ).resized
-        bus.aw.len := 0
-        // data channel
-        bus.w.valid.clear()
-        when(bus.aw.fire){goto(done_data_shot)}
-      }
-
-    done_data_shot
-      .whenIsActive {
-        en.clear()
-        // address channel
-        bus.aw.valid.clear()
-        // data channel
-        bus.w.valid.set()
-        bus.w.data := 1
-
-        when(bus.w.fire){exitFsm()}
-      }
-  }
-
-  /**
-   * function of FSM to drive data into AXI4 bus in burst mode
    * @param addr Burst transaction initial address
    * @param bus The master bus
    * @param burst_len The number of burst transaction
    * @param shot_len The length of burst transaction
    * @return return the corresponding FSM
    */
-  def burstDriverFSM(addr: Int, done_addr: Int, bus: Axi4WriteOnly, burst_len: Int, shot_len: Int) = new StateMachine {
+  def burstDriverFSM(addr: Int, done_addr: Int, bus: Axi4WriteOnly, burst_len: Int, shot_len: Int, fsm_name: String) = new StateMachine {
+    setCompositeName(innerMem, s"${fsm_name}_fsm")
     val burst_cnt = Counter(0 until burst_len).setCompositeName(innerMem, "burst_cnt") // counter for the index burst shot
     val shot_cnt = Counter(0 until shot_len).setCompositeName(innerMem, "shot_cnt") // counter for current index inside a burst transaction
     val burst_prim_addr = RegInit(U(addr, bus.aw.addr.getBitsWidth bit))
