d928a2f1778256e4e69eeb4a84bfd3b21bab8955
Zhengpeng Liao <wswslzp@outlook.com>
2020-12-26 17:38:05 +0800

    update daily

--- src/main/scala/Fpga/Driver/DataTransform.scala
+++ src/main/scala/Fpga/Driver/DataTransform.scala
@@ -5,21 +5,22 @@ import breeze.math._
 import spinal.core._
 
 trait DataTransform {
-  protected def complexToBits(dat: Complex, cfg: HComplexConfig): Bits = {
-    val real_p = (dat.real * scala.math.pow(2, cfg.fracw)).toInt
-    val imag_p = (dat.imag * scala.math.pow(2, cfg.fracw)).toInt
-    if (cfg.real_high) {
-//      B(real_p, cfg.getDataWidth bit) ## B(imag_p, cfg.getDataWidth bit)
-      ( S(real_p, cfg.getDataWidth bit) ## S(imag_p, cfg.getDataWidth bit) ).asBits
-    } else {
-//      B(imag_p, cfg.getDataWidth bit) ## B(real_p, cfg.getDataWidth bit)
-      ( S(imag_p, cfg.getDataWidth bit) ## S(real_p, cfg.getDataWidth bit) ).asBits
-    }
+  protected def doubleToBits(dat: Double, cfg: HComplexConfig): Bits = {
+    val max_v = ( (1L << (cfg.getDataWidth-1))-1 ) / cfg.fracw.toDouble
+    val min_v = -(1L << (cfg.getDataWidth-1)) / cfg.fracw.toDouble
+    val dat1 = scala.math.max(scala.math.min(dat, max_v), min_v)
+    S(
+      ( dat1 * scala.math.pow(2, cfg.fracw) ).toInt, cfg.getDataWidth bit
+    ).asBits
   }
 
-  protected def doubleToBits(dat: Double, cfg: HComplexConfig): Bits = {
-    S(
-      ( dat * scala.math.pow(2, cfg.fracw) ).toInt, cfg.getDataWidth bit
-    ).asBits
+  protected def complexToBits(dat: Complex, cfg: HComplexConfig): Bits = {
+    val real_p = doubleToBits(dat.real, cfg)
+    val imag_p = doubleToBits(dat.imag, cfg)
+    if (cfg.real_high) {
+      real_p ## imag_p
+    } else {
+      imag_p ## real_p
+    }
   }
 }
