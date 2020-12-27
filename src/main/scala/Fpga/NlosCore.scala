f6f33887dcdda92ea6f0ba654a90498f9ec44f83
Zhengpeng Liao <wswslzp@outlook.com>
2020-12-27 17:38:18 +0800

    update daily

--- src/main/scala/Fpga/NlosCore.scala
+++ src/main/scala/Fpga/NlosCore.scala
@@ -78,7 +78,8 @@ case class NlosCore(cfg: RsdKernelConfig)(implicit val axi_config: Axi4Config) e
   fft2d_core.io.data_from_mac << mac_array.io.mac_result
   mac_array.io.mac_result.simPublic()
 
-  io.done := (io.dc === ((1 << io.dc.getWidth)-1)) && (io.fc === ((1<<io.fc.getWidth)-1)) && io.result.valid.fall(False)
+//  io.done := (io.dc === ((1 << io.dc.getWidth)-1)) && (io.fc === ((1<<io.fc.getWidth)-1)) && io.result.valid.fall(False)
+  io.done := ( io.dc === cfg.depth_factor ) && (io.fc === cfg.freq_factor) && io.result.valid.fall(False)
   fft2d_core.io.done := io.done // todo
   val loadUnitAddrs = rgca.loadUnitAddrs
 }
