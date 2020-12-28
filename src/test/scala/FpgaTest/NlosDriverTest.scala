ec2c891a631270b010c1d31a647d173951d350f7
Zhengpeng Liao <wswslzp@outlook.com>
2020-12-28 16:07:38 +0800

    update daily

--- src/test/scala/FpgaTest/NlosDriverTest.scala
+++ src/test/scala/FpgaTest/NlosDriverTest.scala
@@ -190,7 +190,7 @@ object NlosDriverTest extends App{
   csvwrite(new File("tb/NlosDriver/h_wave.csv"), h_wv)
   csvwrite(new File("tb/NlosDriver/h_impulse.csv"), h_imp)
 
-  val h_coef = Computation.generateCoef(wave, h_ds, h_ts)
+  val h_coef = Computation.generateCoef(h_wv, distance, h_ts)
   val h_rsd = Computation.generateRSDRadKernel(h_coef, h_imp)
 //  val uin_fft = h_img.map(fourierTr(_))
   val kernel_size = (uin.head.rows, uin.head.cols)
