d6fbc411b6ae6147186b8d28577fa52c68c0eb39
Zhengpeng Liao <wswslzp@outlook.com>
2021-01-10 17:01:35 +0800

    update daily

--- src/main/scala/Fpga/NlosFpgaCore.scala
+++ src/main/scala/Fpga/NlosFpgaCore.scala
@@ -16,6 +16,7 @@ case class NlosFpgaCore(cfg: RsdKernelConfig, pixel_parallel: Int = 1) extends C
   val nlos_no_driver = NlosNoDriver(cfg)
   val nlos_driver = NlosDriver(cfg, nlos_no_driver.loadUnitAddress)
   val hdmi_if = StreamToHDMI(vid_1280x720p60, cfg.rows, cfg.cols)
+  val stream_forker = StreamFork2(nlos_no_driver.io.result)
 
   nlos_no_driver.io.img_in << nlos_driver.io.original_img
   nlos_no_driver.io.data_in << nlos_driver.io.kernel_in
@@ -25,8 +26,8 @@ case class NlosFpgaCore(cfg: RsdKernelConfig, pixel_parallel: Int = 1) extends C
   nlos_no_driver.io.cnt_incr <> nlos_driver.io.cnt_incr
   nlos_driver.io.sys_init := io.sys_init
   nlos_driver.io.done := nlos_no_driver.io.done
-  io.result << nlos_no_driver.io.result
-  hdmi_if.io.dat_in << nlos_no_driver.io.result
+  io.result << stream_forker._1
+  hdmi_if.io.dat_in << stream_forker._2
   io.hdmi_vid_bus <> hdmi_if.io.vid
   io.done := nlos_no_driver.io.done
 
