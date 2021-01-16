c196c5b5e1ae293f9ac16ef864f15b04971f48c9
Zhengpeng Liao <wswslzp@outlook.com>
2021-01-16 16:49:07 +0800

    update daily

--- src/main/scala/Config/RsdKernelConfig.scala
+++ src/main/scala/Config/RsdKernelConfig.scala
@@ -103,8 +103,8 @@ object RsdKernelConfig {
   )
   val uin = Array.tabulate(rsd_cfg.freq_factor){idx=>
     LoadData.loadComplexMatrix(
-      real_part_filename = s"src/test/resource/data/real/uin_${idx+1}.csv",
-      imag_part_filename = s"src/test/resource/data/imag/uin_${idx+1}.csv"
+      real_part_filename = s"src/test/resource/uin/nlos_letters/real/uin_${idx+1}.csv",
+      imag_part_filename = s"src/test/resource/uin/nlos_letters/imag/uin_${idx+1}.csv"
     )
   }
   implicit val axi_config: Axi4Config = Axi4Config(
