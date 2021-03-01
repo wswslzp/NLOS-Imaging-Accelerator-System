e70ea81b485e63ee72d662bdb25bf7931ed2b686
Zhengpeng Liao <wswslzp@outlook.com>
2021-03-01 16:26:50 +0800

    update daily

--- src/main/scala/Fpga/PostProcess.scala
+++ src/main/scala/Fpga/PostProcess.scala
@@ -73,21 +73,22 @@ case class PostProcess(
     pixel_addr.increment()
   }
 
-  val mem_raddr_sel = Bool()
-  val output_pix_addr = UInt(32 bit)
-  val mem_rdata = result_mem.readSync(
-    address = ( mem_raddr_sel ? output_pix_addr | pixel_addr.value ).resized
-  )
+//  val mem_raddr_sel = Bool()
+//  val output_pix_addr = UInt(32 bit)
+//  val mem_rdata = cloneOf(img_in_q.payload)
+//  mem_rdata := result_mem.readSync( // todo uninferred RAM, due to async read logic. Why ??
+//    address = ( mem_raddr_sel ? output_pix_addr | pixel_addr.value ).resized
+//  )
 
   val q_pix = RegNext(img_in_q.payload)
-//  val mem_prev_pix = result_mem.readSync(pixel_addr)
-//  val qin_larger = mem_prev_pix < q_pix
-  val qin_larger = mem_rdata < q_pix
+  val mem_prev_pix = result_mem.readSync(pixel_addr)
+  val qin_larger = mem_prev_pix < q_pix
+//  val qin_larger = mem_rdata < q_pix
   val store_in_en = RegNext(img_in_q.valid) init False
   val w_pixel_addr = RegNext(pixel_addr.value) init 0
   result_mem.write(
     address = w_pixel_addr,
-    data = qin_larger ? q_pix | mem_rdata,
+    data = qin_larger ? q_pix | mem_prev_pix,
     enable = store_in_en
   )
 
@@ -106,24 +107,24 @@ case class PostProcess(
   }
 
   val pixel_cnt = Counter(0 until cfg.kernel_size.product * over_sample_factor * over_sample_factor)
-//  val output_pix_addr = addressTrans(pixel_cnt.value)
-  output_pix_addr := addressTrans(pixel_cnt.value).resized
+  val output_pix_addr = addressTrans(pixel_cnt.value)
+//  output_pix_addr := addressTrans(pixel_cnt.value).resized
 
   //  `result_ready` signal that all NLOS results have been stored and is ready to output
   //  val result_ready_prev = nlos_comp_done & Reg(Bool()).init(False).setWhen(img_in_q.valid.fall(False))
   val result_ready_prev = nlos_comp_done & Reg(Bool()).init(False).setWhen(store_in_en.fall(False)).clearWhen(pixel_cnt.willOverflow)
   val result_ready = RegNext(result_ready_prev) init False
-  mem_raddr_sel := result_ready
+//  mem_raddr_sel := result_ready
   val pix_bfq_valid = RegNext(result_ready) init False
   val quan_cfg = HComplexConfig(img_in_q.payload.maxExp, -img_in_q.payload.minExp)
   val quantizer = PixelQuant(quan_cfg, quant_bit_width)
-//  val pix_bfq = result_mem.readSync(output_pix_addr.resized)
-//  pix_bfq.setName("pix_bfq")
+  val pix_bfq = result_mem.readSync(output_pix_addr.resized)
+  pix_bfq.setName("pix_bfq")
   quantizer.io.upper_bound := img_in_abs_max
   quantizer.io.lower_bound := img_in_abs_min
   quantizer.io.pix_in.valid := pix_bfq_valid
-//  quantizer.io.pix_in.payload := pix_bfq // todo pix_bfq has value but the io_pix_in_payload is zero constant?
-  quantizer.io.pix_in.payload := mem_rdata
+  quantizer.io.pix_in.payload := pix_bfq // todo pix_bfq has value but the io_pix_in_payload is zero constant?
+//  quantizer.io.pix_in.payload := mem_rdata
   io.img_out.payload := quantizer.io.pix_out.payload
   io.img_out.valid := quantizer.io.pix_out.valid
   when(io.img_out.fire){
