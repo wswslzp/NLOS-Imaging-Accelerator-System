9c70acb80e68710e311ad57e9075bfb341d94917
Zhengpeng Liao <wswslzp@outlook.com>
2021-05-29 11:38:09 +0800

    update daily

--- src/test/scala/FpgaTest/NlosTestAll.scala
+++ src/test/scala/FpgaTest/NlosTestAll.scala
@@ -50,6 +50,7 @@ object NlosTestAll extends App {
   all_data_set.foreach{ds=>
     val dataset_name = ds.pathToData.split("/").last
     println(s"Testing dataset: $dataset_name")
+    scala.sys.wait(10)
     testOnDataset(ds)
     testPostProc(uout_pp, ds, "tmp/result_imgs")
   }
