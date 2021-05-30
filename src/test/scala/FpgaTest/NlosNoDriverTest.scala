d22a2aa4530bb1aee4d168d4588a3bcdffb6ee87
Zhengpeng Liao <wswslzp@outlook.com>
2021-05-30 16:43:38 +0800

    update daily

--- src/test/scala/FpgaTest/NlosNoDriverTest.scala
+++ src/test/scala/FpgaTest/NlosNoDriverTest.scala
@@ -34,7 +34,7 @@ object NlosNoDriverTest extends App{
       .compile(NlosNoDriver(rsd_cfg))
   }
 
-  val uout_pp = DenseMatrix.zeros[Double](rsd_cfg.kernel_size.head*2, rsd_cfg.kernel_size.last*2)
+  val uout_pp = DenseMatrix.zeros[Double](rsd_cfg.kernel_size.head*1, rsd_cfg.kernel_size.last*1)
 
     compiled.doSim("NlosNoDriver_tb") { dut =>
       dut.clockDomain.forkStimulus(2)
